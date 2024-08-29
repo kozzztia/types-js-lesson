2d061475e2b0380e4a7c20d508efdc13af73010a
kozzztia <kozzztia@gmail.com>
2024-08-29 13:19:19 +0300

    first version

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("start")
+
+const a = "0,1";
+const b = "0,2";
+
+function correction(arg){
+    return +arg.split(",").join('.');
+}
+
+function sum(...args){
+    const result = +args.reduce((acc,item) => acc + correction(item),0).toFixed(1)
+    console.log(result)
+}
+
+sum(a,b)
