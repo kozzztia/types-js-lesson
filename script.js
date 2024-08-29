2f4a02716fe81eb0634a40ee6ceb2ffab20099c7
kozzztia <kozzztia@gmail.com>
2024-08-29 13:22:43 +0300

    second question

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 console.log("start")
 
-const a = "0,1";
-const b = "0,2";
+// const a = "0,1";
+// const b = "0,2";
 
-function correction(arg){
-    return +arg.split(",").join('.');
-}
+// function correction(arg){
+//     return +arg.split(",").join('.');
+// }
 
-function sum(...args){
-    const result = +args.reduce((acc,item) => acc + correction(item),0).toFixed(1)
-    console.log(result)
-}
+// function sum(...args){
+//     const result = +args.reduce((acc,item) => acc + correction(item),0).toFixed(1)
+//     console.log(result)
+// }
 
-sum(a,b)
+// sum(a,b)
+
+const c = "1";
+const d = 2;
+
+console.log(+c + +d)
+console.log(Number(c) + Number(d))
